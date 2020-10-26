ee98b8de5dd55c51e07807a8a6eee03e87f93073
DWShuo <errorcodes@tuta.io>
2020-10-26 16:06:27 -0400

    updated deploy script to install packages on first run

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 OG_PWD=$PWD
+NEW_INSTALL=0
 
 function buildMain {
   cd $OG_PWD/main/
+  
+  if (($NEW_INSTALL == 1)); then
+    npm install
+  fi
+
   echo building $PWD ...
   gatsby build
 }
 
 function buildSub {
   cd $OG_PWD/blog/
+
+  if (($NEW_INSTALL == 1)); then
+    npm install
+  fi
+
   echo building $PWD ...
   gatsby build --prefix-paths
 }
+
+if [[ -d $OG_PWD/main/node_modules ]] && [[ -d $OG_PWD/blog/node_modules ]] ; then
+    NEW_INSTALL=0
+  else
+    NEW_INSTALL=1
+fi
 
 buildMain 
 buildSub
