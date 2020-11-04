72154181a2bf44c3dfe0d839dca6489b01350843
dwshuo <errorcodes@tuta.io>
2020-11-04 00:11:52 -0800

    changed menu title

--- blog/src/gatsby-theme-blog/components/sidebar.js
+++ blog/src/gatsby-theme-blog/components/sidebar.js
@@ -7,7 +7,7 @@ export default props => {
         // Pass on our props
         <Menu {...props} id="push" right>
             <a aria-current="page" className="bm-item Header__menu__item menuTitle" style={{ display: 'block' }} 
-              tabindex="0" href="/">DWSHUO</a>
+              tabindex="0" href="/">DWS</a>
             <a className="menu-item Header__menu__item" href="/"> Home </a>
 
             <a className="menu-item Header__menu__item" href="/blog"> Blog </a>
